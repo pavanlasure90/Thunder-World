478ea61b692d4c1f31ae9a0c67d18d29ba113612
pavanlasure90 <113824942+pavanlasure90@users.noreply.github.com>
2024-03-21 11:16:40 +0530

    searchbar width

--- client/thunder-world/src/pages/Home.jsx
+++ client/thunder-world/src/pages/Home.jsx
@@ -1,106 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import Navbar from '../Components/Navbar';
-// import './Home.css';
-
-// const Home = () => {
-//   const [originalProducts, setOriginalProducts] = useState([]);
-//   const [products, setProducts] = useState([]);
-//   const [searchQuery, setSearchQuery] = useState('');
-
-//   useEffect(() => {
-//     fetch('https://fakestoreapi.com/products?limit=20')
-//       .then((response) => response.json())
-//       .then((data) => {
-//         const initialProducts = data.map((product) => ({
-//           title: product.title,
-//           image: product.image,
-//           description: product.description,
-//           showFullDescription: false,
-//         }));
-//         setOriginalProducts(initialProducts);
-//         setProducts(initialProducts);
-//       });
-//   }, []);
-
-//   const handleSearch = (query) => {
-//     const filteredProducts = originalProducts.filter(
-//       (product) => product.title.toLowerCase().includes(query.toLowerCase())
-//     );
-//     setProducts(filteredProducts);
-//   };
-
-//   const handleChange = (e) => {
-//     const query = e.target.value;
-//     setSearchQuery(query);
-//     handleSearch(query);
-//   };
-
-//   return (
-//     <>
-//       <Navbar />
-//       <div className="container my-3">
-//         <form className="form-inline my-2 my-lg-0">
-//           <input
-//             className="form-control mr-sm-2"
-//             type="search"
-//             placeholder="Search"
-//             aria-label="Search"
-//             value={searchQuery}
-//             onChange={handleChange}
-//           />
-//         </form>
-//       </div>
-//       <div className="container">
-//         <div className="row">
-//           {products.map((product, index) => (
-//             <div key={index} className="col-md-4 mb-3">
-//               <div className="card">
-//                 <img src={product.image} className="card-img-top" alt="" />
-//                 <div className="card-body">
-//                   <h3 className="card-title">{product.title}</h3>
-//                   <p className="card-text">
-//                     {product.showFullDescription
-//                       ? product.description
-//                       : `${product.description.slice(0, 80)}...`}
-//                     <span
-//                       className="read-more-link"
-//                       onClick={() => {
-//                         const updatedProducts = products.map((p, i) => {
-//                           if (i === index) {
-//                             return {
-//                               title: product.title,
-//                               image: product.image,
-//                               description: product.description,
-//                               showFullDescription: !product.showFullDescription,
-//                             };
-//                           }
-//                           return;
-//                         });
-//                         setProducts(updatedProducts);
-//                       }}
-//                     >
-//                       {product.showFullDescription ? 'Read less' : 'Read more'}
-//                     </span>
-//                   </p>
-//                   <div className="d-flex justify-content-between">
-//                     <button className="btn btn-outline-primary">Add to cart</button>
-//                     <button className="btn btn-primary">Buy now</button>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Home;
-
-
-
-
 import React, { useEffect, useState } from 'react';
 import Navbar from '../Components/Navbar';
 import axios from 'axios';
@@ -156,6 +53,7 @@ const Home = () => {
       <div className="container my-3">
         <form className="form-inline my-2 my-lg-0">
           <input
+            style={{width:"100vw"}}
             className="form-control mr-sm-2"
             type="search"
             placeholder="Search"
