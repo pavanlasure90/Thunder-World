2d5b0c7f926f002c19e154c093fbe75cac9eb62e
pavanlasure90 <113824942+pavanlasure90@users.noreply.github.com>
2024-05-08 14:54:10 +0530

    .

--- client/thunder-world/src/pages/Home.jsx
+++ client/thunder-world/src/pages/Home.jsx
@@ -103,7 +103,7 @@ const Home = () => {
                       {product.showFullDescription ? 'Read less' : 'Read more'}
                     </span>
                   </p>
-                  <p className="card-price">Price: {product.price}</p>
+                  <p className="card-price">Price: ${product.price}</p>
                   <div className="d-flex justify-content-between">
                     <button className="btn btn-outline-primary" onClick={() => handleAddToCart(product)}>Add to cart</button>
                     <button className="btn btn-primary">Buy now</button>
